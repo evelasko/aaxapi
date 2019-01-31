cff4e4287a035c9427a0129930a0c58469395175
Enrique Velasco <h.superpotter@gmail.com>
2019-01-31 01:47:41 +0100

    cors netlify site

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Mailgun from 'mailgun-js'
 const options = {
   port: process.env.PORT || 4000,
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost', 'http://localhost:4000'],
+    origin: ['http://localhost:3000', 'http://localhost', 'http://localhost:4000', 'https://aaxadmin.netlify.com'],
     credentials: true,
     optionsSuccessStatus: 200
   } // some legacy browsers (IE11, various SmartTVs) choke on 204
