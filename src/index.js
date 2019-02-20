15ddb50eb38113822c2a28ffdf77760c58468aee
Enrique Velasco <h.superpotter@gmail.com>
2019-02-20 02:09:45 +0100

    new cors config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import '@babel/polyfill/noConflict'
-import server from './server'
-import initScheduleJob from './utils/scheduler'
-import { initFullCache } from './cache'
-import { getGroupRequests, getUserByEmail, getNewsById } from './utils/queryCache'
+import '@babel/polyfill/noConflict';
+import { initFullCache } from './cache';
+import server from './server';
+import initScheduleJob from './utils/scheduler';
 
 const port = parseInt(process.env.PORT, 10) || 4000
 const options = {
   port,
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost', 'http://localhost:4000', 'https://aaxadmin.netlify.com', 'https://admin.alicialonso.org'],
+    origin: [
+      'http://localhost:3000', 
+      'http://localhost', 
+      'http://localhost:4000', 
+      'https://aaxadmin.netlify.com', 
+      'https://admin.alicialonso.org'],
     credentials: true,
     optionsSuccessStatus: 200
   } // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -17,5 +21,6 @@ const options = {
 
 initFullCache().then(res => {
   initScheduleJob() // init scheduler for outdated nodes
-  server.start(options, () => { console.log('Server up and running at port: ', port || 4000) })
+  
 })
+server.start(options, () => { console.log('Server up and running at port: ', port || 4000) })
