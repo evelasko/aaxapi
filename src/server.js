e41ed177c02d25a5d2f6d781bb5f3d42952001eb
Enrique Velasco <h.superpotter@gmail.com>
2019-03-13 21:07:58 +0100

    redis config

--- src/server.js
+++ src/server.js
@@ -1,28 +1,25 @@
 import connectRedis from 'connect-redis';
 import express from 'express';
 import session from 'express-session';
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 // const RedisStore = require('connect-redis')(session)
 import Redis from 'ioredis';
 import { redisSessionPrefix } from './constants';
 import prisma from './prisma';
-// import { ApolloEngine } from 'apollo-engine'
 import { fragmentReplacements, resolvers, typeDefs } from './schema';
 
 export const redis = new Redis(process.env.REDIS_URL)
-// const pubsub = new PubSub()
+const pubsub = new PubSub()
 // const options = {
 //   retry_strategy: options => {
 //     // reconnect after
 //     return Math.max(options.attempt * 100, 3000);
 //   }
 // }
-const pubsub = new RedisPubSub({
-  publisher: new Redis(),
-  subscriber: new Redis()
-})
-
+// const pubsub = new RedisPubSub({
+//   publisher: new Redis(),
+//   subscriber: new Redis()
+// })
 
 const server = new GraphQLServer({
     typeDefs,
@@ -58,11 +55,6 @@ server.express.use(session(
 )
 server.express.use('/images', express.static('images'))
 server.express.use('/resources', express.static('resources'))
-
-// const engine = new ApolloEngine({ apiKey: process.env.ENGINE_API_KEY })
-// const port = parseInt(process.env.PORT, 10) || 4000
-// engine.listen({ port, graphqlPaths: ['/'], expressApp: server.express, launcherOptions: { startupTimeout: 3000 }, },
-//               () => { console.log('Apollo Engine Listening!') })
 
 export { server as default };
 
